374d2e9369ba9a06fb5520896c16b02af13ea7bb
Zachariah Nagatani <zachnagatani@gmail.com>
2017-06-09 15:57:58 -0700

    Initial Router set up and sidebar

---
+++ public/app/src/components/Sidebar/Sidebar.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default function Sidebar(props) {
+    return (
+        <nav className="sidebar float-left">
+            <ul className="sidebar__nav menu vertical">
+                <li className="sidebar__brand menu-text">Issue Tracker</li>
+                <li className="sidebar__link">Issue Data</li>
+                <li className="sidebar__link">
+                    <a href="/logout">Logout</a>
+                </li>
+            </ul>
+        </nav>
+    );
+};
