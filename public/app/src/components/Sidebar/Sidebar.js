74b483cdfa569e8bd00e025a981229c6f22088bf
Zachariah Nagatani <zachnagatani@gmail.com>
2017-06-09 16:08:35 -0700

    Add Foundation/Normalize and more sidebar styles

--- public/app/src/components/Sidebar/Sidebar.js
+++ public/app/src/components/Sidebar/Sidebar.js
@@ -5,7 +5,9 @@ export default function Sidebar(props) {
         <nav className="sidebar float-left">
             <ul className="sidebar__nav menu vertical">
                 <li className="sidebar__brand menu-text">Issue Tracker</li>
-                <li className="sidebar__link">Issue Data</li>
+                <li className="sidebar__link">
+                    <a href="#">Issue Data</a>
+                </li>
                 <li className="sidebar__link">
                     <a href="/logout">Logout</a>
                 </li>
