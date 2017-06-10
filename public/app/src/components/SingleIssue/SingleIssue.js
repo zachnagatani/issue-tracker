005b1a1ff95eb3bfbc0f01459c454cec5f4c11cb
Zachariah Nagatani <zachnagatani@gmail.com>
2017-06-09 19:50:26 -0700

    Single Issue component template

--- public/app/src/components/SingleIssue/SingleIssue.js
+++ public/app/src/components/SingleIssue/SingleIssue.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function SingleIssue(props) {
     return (
-        <div>
-            SingleIssue!
+        <div className="issue-container">
+            <div className="card text-center">
+                <div className="card-divider">
+                    <h1>Issue {props.id}</h1>
+                </div>
+                <div className="card-section">
+                    <p>Submitted by: John Doe</p>
+                    <p>Email: johndoe@example.com</p>
+                    <p>Status: Open</p>
+                    <p>Opened On: 6/9/17</p>
+                    <p>Days Open: 1</p>
+                    <p className="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, animi, omnis magni error in id nulla illum nisi iure, enim debitis voluptates doloremque neque deserunt quasi expedita accusantium! Cupiditate, dolor.</p>
+
+                    <div className="expanded button-group">
+                        <button type="button" className="primary button">Close Issue</button>
+                        <Link to="/issues" type="button" className="secondary button">Back to Issues</Link>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
