0c1b4430dd7bd7bbf63d5fdea2b981e2908ad596
Zachariah Nagatani <zachnagatani@gmail.com>
2017-06-09 20:32:50 -0700

    Mock data for single issue

--- public/app/src/components/SingleIssue/SingleIssue.js
+++ public/app/src/components/SingleIssue/SingleIssue.js
@@ -2,18 +2,31 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default function SingleIssue(props) {
+    const issue = {
+                "id": "5",
+                "type": "issue",
+                "attributes": {
+                    "customerName": "Sargent Guichard",
+                    "customerEmail": "byurevich0@cnn.com",
+                    "description": "sapiendsfdsfsdivamus metus arcu adipiscing molestie hendrerit at vulputate vitae nisl aenean lectus pellentesque eget",
+                    "status": true,
+                    "openedAt": "01/02/2017",
+                    "closedAt": "01/04/2017",
+                    "handling-employee": "Zach Nagatani"
+                }
+            };
+
     return (
         <div className="issue-container">
             <div className="card text-center">
                 <div className="card-divider">
-                    <h1>Issue {props.id}</h1>
+                    <h1>Issue {issue.id}</h1>
                 </div>
                 <div className="card-section">
-                    <p>Submitted by: John Doe</p>
-                    <p>Email: johndoe@example.com</p>
+                    <p>Submitted by: {issue.attributes.customerName}</p>
+                    <p>Email: {issue.attributes.customerEmail}</p>
                     <p>Status: Open</p>
-                    <p>Opened On: 6/9/17</p>
-                    <p>Days Open: 1</p>
+                    <p>Opened On: {issue.attributes.openedAt}</p>
                     <p className="text-left">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus, animi, omnis magni error in id nulla illum nisi iure, enim debitis voluptates doloremque neque deserunt quasi expedita accusantium! Cupiditate, dolor.</p>
 
                     <div className="expanded button-group">
