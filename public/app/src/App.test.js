0f34c904f8e8a418d66a41a4e1d4d33137febabd
Zachariah Nagatani <zachnagatani@gmail.com>
2017-06-10 17:32:10 -0700

    Fix broken tests with shallow rendering

--- public/app/src/App.test.js
+++ public/app/src/App.test.js
@@ -1,15 +1,8 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import ReactTestUtils from 'react-dom/test-utils';
 
 import App from './App';
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>,
-      div
-  );
+  ReactTestUtils.createRenderer().render(<App />);
 });
